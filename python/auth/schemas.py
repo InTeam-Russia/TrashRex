c701f563a7b3bc84fd55302ced7f3174e66e4af3
ProstoLive <artem009saratov@gmail.com>
2024-05-03 15:17:04 +0400

    Added endpoints related to problems

--- python/auth/schemas.py
+++ python/auth/schemas.py
@@ -15,7 +15,6 @@ class UserRead(schemas.BaseUser[int]):
 
 class UserCreate(schemas.BaseUserCreate):
     email: str
-    hashed_password: str
     is_active: Optional[bool] = True
     is_superuser: Optional[bool] = False
     is_verified: Optional[bool] = False
--- python/main.py
+++ python/main.py
@@ -6,13 +6,15 @@ from fastapi.responses import JSONResponse
 from sqlalchemy import text, insert, select
 
 from auth.auth import auth_backend
-from auth.database import User, engine, DATABASE_URL
+from auth.database import User, engine, DATABASE_URL, get_async_session
 from auth.manager import get_user_manager
 from auth.schemas import UserRead, UserCreate
 
 from models.models import problems
 
+
 app = FastAPI()
+session = get_async_session()
 
 fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
@@ -21,12 +23,12 @@ fastapi_users = FastAPIUsers[User, int](
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth",
+    prefix="/user",
     tags=["auth"],
 )
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
+    prefix="/user",
     tags=["auth"],
 )
 
@@ -35,3 +37,32 @@ current_user = fastapi_users.current_user()
 @app.get("/protected-route")
 def protected_route(user: User = Depends(current_user)):
     return f"Hello, {user.username}"
+
+@app.post("/problems/create")
+async def create_problem(description: str, problem_photo: str, lat: float, lon: float, user: User = Depends(current_user)):
+    q = insert(problems).values(description = description, problem_photo = problem_photo, lat = lat, lon = lon, author_id = user.id)
+    await session.execute(q)
+    await session.commit()
+
+@app.post("/problems/join/{problem_id}")
+async def join_problem(problem_id: int, user: User = Depends(current_user)):
+    problem = problems.query.filter_by(problem_id = problem_id).first()
+    problem.author_id = user.id
+    problem.state = "in_progress"
+    await session.commit()
+
+@app.post("/problems/{command}/{id}")
+async def create_command(command: str, id: int, user: User = Depends(current_user)):
+    problem = problems.query.filter_by(problem_id = problem_id).first()
+    if command == "finish":
+        problem.state = "completed"
+    elif command == "verificate":
+        problem.state = "on_verification"
+    elif command == "voting":
+        problem.state = "on_voting"
+    await session.commit()
+
+@app.get("/problems/all")
+async def all_problems(user: User = Depends(current_user)):
+    problems = problems.query.all()
+    return JSONResponse(problems)
