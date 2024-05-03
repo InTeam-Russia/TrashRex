a1780011b725f6146d82e2bd987cd51fcdc86826
ProstoLive <artem009saratov@gmail.com>
2024-05-04 01:06:19 +0400

    Made code more readable, even in swagger

--- python/main.py
+++ python/main.py
@@ -1,18 +1,16 @@
 import json
 
-from fastapi import FastAPI, Path, Query, Depends, HTTPException, status
-from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi_users import FastAPIUsers
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 
-from sqlalchemy import text, insert, select, update, delete
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text, insert, select, update
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from auth.auth import auth_backend
-from auth.database import User, engine, DATABASE_URL
+from auth.database import User, engine
 from auth.manager import get_user_manager
 from auth.schemas import UserRead, UserCreate
 
@@ -30,7 +28,9 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allow_headers=["Content-Type", "Authorization", "Set-Cookie", "Access-Control-Request-Headers", "Access-Control-Allow-Headers"],
+    allow_headers=["Content-Type", "Authorization",
+                   "Set-Cookie", "Access-Control-Request-Headers",
+                   "Access-Control-Allow-Headers"],
 )
 
 fastapi_users = FastAPIUsers[User, int](
@@ -51,61 +51,97 @@ app.include_router(
 
 current_user = fastapi_users.current_user()
 
-@app.get("/protected-route")
-def protected_route(user: User = Depends(current_user)):
-    return f"Hello, {user.id}"
+@app.post("/problems/create",
+          tags=["Problems"],
+          summary="Route for creating a problem"
+          )
+async def create_problem(
+        description: str, photo: str, lat: float, lon: float,
+        user: User = Depends(current_user)):
 
-@app.post("/problems/create")
-async def create_problem(description: str, photo: str, lat: float, lon: float, user: User = Depends(current_user)):
     async with Async_Session() as session:
-        q = insert(problems).values(description=description, photo= photo, lat=lat, lon=lon, author_id=user.id, solver_id=None, solution_photo=None)
+        q = insert(problems).values(
+            description=description,
+            photo= photo,
+            lat=lat,
+            lon=lon,
+            author_id=user.id,
+            solver_id=None,
+            solution_photo=None
+        )
         await session.execute(q)
         await session.commit()
-        return JSONResponse(content="Done!", status_code=status.HTTP_201_CREATED)
+        return JSONResponse(
+                content={
+                    "description:": description,
+                    "photo": photo,
+                    "author_id": user.id,
+                    "solver_id": None,
+                    "solution_photo": None
+                },
+                status_code=status.HTTP_201_CREATED
+        )
 
-@app.post("/problems/join/{problem_id}")
+@app.patch("/problems/join/{problem_id}",
+           tags=["Problems"],
+           summary="Route for joining a problem"
+           )
 async def join_problem(problem_id: int, user: User = Depends(current_user)):
     async with Async_Session() as session:
-        await session.execute(update(problems).where(problems.c.id == problem_id).values({"solver_id": user.id, "state": "in_progress"}))
+        await session.execute(
+            update(problems).where(problems.c.id == problem_id).values(
+                {"solver_id": user.id, "state": "in_progress"}
+            )
+        )
         await session.commit()
 
-@app.post("/problems/{command}/{id}")
-async def create_command(command: str, problem_id: int, user: User = Depends(current_user)):
-    async with Async_Session() as session:
-        if command == "finish":
-            await session.execute(update(problems).where(problems.c.id == problem_id).values(
-                {"state": "completed"}))
-        elif command == "verificate":
-            await session.execute(update(problems).where(problems.c.id == problem_id).values(
-                {"state": "on_verification"}))
-        elif command == "voting":
-            await session.execute(update(problems).where(problems.c.id == problem_id).values(
-                {"state": "on_voting"}))
-        await session.commit()
-
-@app.post("/problems/finish/{id}")
+@app.patch("/problems/finish/{id}",
+           tags=["Problems"],
+           summary="Route for finishing a problem"
+           )
 async def finish_problem(problem_id: int, user: User = Depends(current_user)):
     async with Async_Session() as session:
-        await session.execute(update(problems).where(problems.c.id == problem_id).values({"state": "in_progress"}))
+        await session.execute(
+            update(problems).where(problems.c.id == problem_id).values(
+                {"state": "in_progress"}
+            )
+        )
         await session.commit()
 
-@app.post("/problems/verificate/{id}")
+@app.patch("/problems/verificate/{id}",
+           tags=["Problems"],
+           summary="Route for verifying a problem with {id} when it's done."
+           )
+async def verificate_problem(problem_id: int, user: User = Depends(current_user)):
+    async with Async_Session() as session:
+        await session.execute(
+            update(problems).where(problems.c.id == problem_id).values(
+                {"state": "on_verification"}
+            )
+        )
+        await session.commit()
+
+@app.patch("/problems/voting/{id}",
+           tags=["Problems"],
+           summary="Route for placing a voting on a problem with {id}."
+           )
 async def finish_problem(problem_id: int, user: User = Depends(current_user)):
     async with Async_Session() as session:
-        await session.execute(update(problems).where(problems.c.id == problem_id).values({"state": "on_verification"}))
+        await session.execute(
+            update(problems).where(problems.c.id == problem_id).values(
+                {"state": "on_voting"}
+            )
+        )
         await session.commit()
 
-@app.post("/problems/voting/{id}")
-async def finish_problem(problem_id: int, user: User = Depends(current_user)):
-    async with Async_Session() as session:
-        await session.execute(update(problems).where(problems.c.id == problem_id).values({"state": "on_voting"}))
-        await session.commit()
-
-@app.get("/problems/all")
+@app.get("/problems/all",
+         tags=["Problems"],
+         summary="Route for selecting all problems."
+         )
 async def all_problems(user: User = Depends(current_user)):
     async with Async_Session() as session:
-        results = await session.execute(select(problems).order_by(problems.c.id))
-        #for row in results:
-
-        answer = '\n'.join(json.dumps(dict(row._asdict())) for row in results)
+        results = await session.execute(
+            select(problems).order_by(problems.c.id)
+        )
+        answer = (json.dumps(dict(row._asdict())) for row in results)
         return answer
