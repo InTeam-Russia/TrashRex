704e869d79e81fb5d22ec34677c763eff2cc475e
tolstovr <robertproducts2021@gmail.com>
2024-05-03 16:14:53 +0400

    Auth and registration form, no server

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Navbar from "../components/Navbar/Navbar.svelte";
+</script>
+
+<main>
+    <Navbar />
+    <slot />
+</main>
+
+<style lang="scss">
+    @import "../app.scss";
+</style>
