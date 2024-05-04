292979225203f077ea4990168ab0b683e6e0a0a5
tolstovr <robertproducts2021@gmail.com>
2024-05-04 11:43:39 +0400

    Автолоадер бекапа

--- restore_database.sh
+++ restore_database.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-echo "DROP DATABASE logistic; CREATE DATABASE logistic" | psql -h 127.0.0.1 -p 5432 -U postgres
-psql -h 127.0.0.1 -p 5432 -U postgres logistic < backup.sql
+echo "DROP DATABASE trash; CREATE DATABASE trash" | psql -h 127.0.0.1 -p 5432 -U postgres
+psql -h 127.0.0.1 -p 5432 -U postgres trash < backup.sql
