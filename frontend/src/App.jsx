64da0a6968e5cbfbed7f571d6e458f921271908c
tolstovr <robertproducts2021@gmail.com>
2024-05-04 15:50:46 +0400

    АААА ЕБУЧИЙ РЕАКТ

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,38 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
 import AuthregPage from "./pages/AuthregPage/AuthregPage"
 import MapPage from "./pages/MapPage/MapPage"
 import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
+import { MyProblemsPage } from "./pages/MyProblemsPage/MyProblemsPage"
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState({
+    id: 1,
+    email: "tolstovrob@gmail.com",
+    telegram: "https://t.me/im_robertproducts",
+    vk: "https://vk.com/robertproducts",
+    photo: "public/img/defaultUser.webp",
+    name: "Роберт",
+    surname: "Толстов"
+  })
+
   const location = useLocation()
 
-  const getCurrentUser = async () => {
-    return await fetch("http://10.1.0.101:8000/user/whoami")
-    .then((res) => (res.ok ? res.json() : false))
-    .catch(() => {
-      alert("Ошибка сервера, извините :(")
-      return false
-    })
+  const getCurrentUser = () => {
+    // await fetch("http://10.1.0.101:8000/auth/whoami")
+    // .then((response) => {
+    //   return new Promise((response) => resolve({
+    //       status: response.status,
+    //       ok: response.ok,
+    //       response,
+    //     })));
+    // }).then(({ status, response, ok }) => {
+    //   if(ok) {
+    //     setUser(await response.json())
+    //   }
+    // })
   }
-
-  useEffect(() => {
-    const asyncGetCurrentUser = async () => {
-      setUser(await getCurrentUser())
-    }
-    asyncGetCurrentUser()
-  }, [location])
 
   return (
     <main>
@@ -33,6 +42,8 @@ const App = () => {
           <Route path="/authreg" element={<AuthregPage />} />
           <Route path="/map" element={<MapPage />} />
           <Route path="/" element={<MapPage />} />
+          <Route path="/problems" element={<MyProblemsPage />} />
+          <Route path="/profile" element={<AuthregPage />} />
         </Routes>
       </div>
     </main>
