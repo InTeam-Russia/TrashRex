d0bffb303be639b581bcf48d09fc432a9c9ec6c2
ProstoLive <artem009saratov@gmail.com>
2024-05-05 02:26:58 +0400

    Secret phrase added

--- python/auth/manager.py
+++ python/auth/manager.py
@@ -4,9 +4,10 @@ from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, IntegerIDMixin, schemas, models
 from fastapi_users.exceptions import UserAlreadyExists
 
+from config import TOKEN_SECRET_PHRASE
 from .database import User, get_user_db
 
-SECRET = "SECRET"
+SECRET = TOKEN_SECRET_PHRASE
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     reset_password_token_secret = SECRET
     verification_token_secret = SECRET
