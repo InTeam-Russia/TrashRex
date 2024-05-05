71fe1c2becaed1301463e5a97687271272fe62dd
Данила Григорьев <65398685+danilasar@users.noreply.github.com>
2024-05-05 10:30:47 +0400

    Ремонт App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,52 +8,44 @@ import { useLocation } from "react-router-dom"
 import { ProfilePage } from "./pages/ProfilePage/ProfilePage"
 
 const App = () => {
-  const [user, setUser] = useState({
-    email: "tolstovrob@gmail.com",
-    photo: "/img/defaultUser.webp",
-    telegram: "im_robertproducts",
-    vk: "robertproducts",
-    name: "Robert",
-    surname: "Tolstov",
-  })
+  const [user, setUser] = useState(null)
   const location = useLocation()
 
-  // const getCurrentUser = async () => {
-  //   const response = await fetch("http://localhost:8000/auth/whoami", {
-  //     credentials: 'include',
-  //   })
-  //   .then((res) => {
-  //     return res.ok ? res.json() : false
-  //   })
-  //   .catch(() => {
-  //     return false
-  //   })
-  //   return response
-  // }
+  const getCurrentUser = async () => {
+    const response = await fetch("http://localhost:8000/auth/whoami", {
+      credentials: 'include',
+    })
+    .then((res) => {
+      return res.ok ? res.json() : false
+    })
+    .catch(() => {
+      return false
+    })
+    return response
+  }
 
-  // useEffect(() => {
-  //   const asyncGetCurrentUser = async () => {
-  //     setUser(await getCurrentUser())
-  //   }
-  //   asyncGetCurrentUser()
-  // }, [location])
+  useEffect(() => {
+    const asyncGetCurrentUser = async () => {
+      setUser(await getCurrentUser())
+    }
+    asyncGetCurrentUser()
+  }, [location])
 
   return (
     <main>
       <Navbar user={user} setUser={setUser} />
       <div className="app-wrapper">
         <Routes>
+          <Route path="/map" element={<MapPage user={user} setUser={setUser} />} />
           {user ?
           <>
             <Route path="/" element={<MapPage user={user} setUser={setUser} />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/map" element={<MapPage user={user} setUser={setUser} />} />
             <Route path="/profile" element={<ProfilePage curuser={user} />} />
           </>
           :
           <>
             <Route path="/" element={<AboutPage />} />
-            <Route path="/map" element={<MapPage user={user} setUser={setUser} />} />
             <Route path="/authreg" element={<AuthregPage />} />
           </>
           }
